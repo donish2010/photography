99018968aa8047430f39463ccd5f4e0c1c57a8fa
Dmitriy Onishchenko <donish@uw.edu>
2018-03-01 14:10:52 -0800

    changed refering page

--- PHP/contact.php
+++ PHP/contact.php
@@ -3,7 +3,7 @@
 $yourName = 'Dmitriy Onishchenko';
 $yourEmail = 'd.onish2010@gmail.com';
 $yourSubject = 'REQUEST MORE INFO';
-$referringPage = 'home2.html';
+$referringPage = 'index.html';
 // No need to edit below unless you really want to. It's using a simple php mail() function. Use your own if you want
 function cleanPosUrl ($str) {
 return stripslashes($str);
